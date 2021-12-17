06162b1e399313bec8d85ccf831596857dd81361
plainchn <plainwe@gmail.com>
2021-12-17 18:05:47 +0800

    add shell sort

--- 排序算法/src/main.cpp
+++ 排序算法/src/main.cpp
@@ -9,60 +9,40 @@
 using namespace std;
 using namespace chrono;
 
-enum sort_type
-{
-  bubble,
-  selection,
-  insertion,
-  quick,
-  heap
-};
 
-#define SIZE 100000000
-sort_type type= heap;
+#define SIZE 80000000
+sort_type type= shell;
 int main()
 {
   high_resolution_clock::time_point start, end;
   duration<double> time_span;
 
   vector<int> arr = geneVector(SIZE);
+
   vector<int> arr1(arr);
   sort(arr1.begin(), arr1.end());
+  // sort(arr.begin(), arr.end());
   // print(arr1);
+
+  start = high_resolution_clock::now();
+  string method;
   switch (type)
   {
-  case bubble:
-    start = high_resolution_clock::now();
-    bubbleSort(arr);
-    end = high_resolution_clock::now();
-    break;
-  case selection:
-    start = high_resolution_clock::now();
-    selectionSort(arr);
-    end = high_resolution_clock::now();
-    break;
-  case insertion:
-    start = high_resolution_clock::now();
-    insertionSort(arr);
-    end = high_resolution_clock::now();
-    break;
-  case quick:
-    start = high_resolution_clock::now();
-    quickSort(arr, 0, SIZE - 1);
-    end = high_resolution_clock::now();
-    break;
-  case heap:
-    start = high_resolution_clock::now();
-    heapSort(arr);
-    end = high_resolution_clock::now();
-    break;
+  case bubble:bubbleSort(arr);method="BubbleSort"s;break;
+  case selection:selectionSort(arr);method="SelectionSort"s;break;
+  case insertion:insertionSort(arr);method="InsertionSort"s;break;
+  case shell:shellSort(arr);method="ShellSort"s;break;
+  case quick:quickSort(arr, 0, SIZE - 1);method="QuickSort"s;break;
+  case merging:mergeSort(arr, 0, SIZE - 1);method="MergeSort"s;break;
+  case heap:heapSort(arr);method="HeapSort"s;break;
   default:break;
   }
-
+  end = high_resolution_clock::now();
   time_span = duration_cast<duration<double>>(end - start);
+
   // print(arr);
   if (arr1 == arr)
-    cout << "Right."   
+    cout << method << "." 
          << "The sorting time is " << time_span.count() << " seconds." 
          << " size = " << SIZE << endl;
   return 0;
--- 排序算法/src/sort.cpp
+++ 排序算法/src/sort.cpp
@@ -28,20 +28,20 @@ void bubbleSort(std::vector<int> &arr)
 void selectionSort(std::vector<int> &arr)
 {
   int n = arr.size();
-  int Min_index = 0;
+  int min_index = 0;
   for (int i = 0; i < n; i++)
   {
-    Min_index = i;
+    min_index = i;
     for (int j = i + 1; j < n; j++)
     {
-      if (arr[j] < arr[Min_index])
-        Min_index = j;
+      if (arr[j] < arr[min_index])
+        min_index = j;
     }
-    if (i != Min_index)
+    if (i != min_index)
     {
       int temp = arr[i];
-      arr[i] = arr[Min_index];
-      arr[Min_index] = temp;
+      arr[i] = arr[min_index];
+      arr[min_index] = temp;
     }
   }
 }
@@ -63,6 +63,30 @@ void insertionSort(std::vector<int> &arr)
   }
 }
 
+void shellSort(std::vector<int> &arr)
+{
+  int len =arr.size();
+
+#ifndef other
+  for(int gap=len/2;gap>0;gap/=2)
+  {
+    for(int i=gap;i <len;i++)
+      for(int j=i-gap;j>=0;j-=gap)
+      {
+        if(arr[j]<arr[j+gap])
+          break;
+        else
+        {
+          std::swap(arr[j],arr[j+gap]);
+        }
+      }
+  }
+#else
+
+#endif
+
+#undef other
+}
 //快速排序
 void quickSort(std::vector<int> &arr, int left, int right)
 {
@@ -93,7 +117,46 @@ void quickSort(std::vector<int> &arr, int left, int right)
     quickSort(arr, i + 1, right);
   }
 }
+// 归并排序
+void merge(std::vector<int> &arr,int l,int m,int r)
+{
+  int L_SIZE=m-l+1;
+  int R_SIZE=r-m;
 
+  std::vector<int>L(L_SIZE);
+  std::vector<int>R(R_SIZE);
+
+  for(int i=0;i<L_SIZE;i++) L[i]=arr[i+l];
+  for(int j=0;j<R_SIZE;j++) R[j]=arr[j+m+1];
+
+  int i=0,j=0,k=l;
+  while(i<L_SIZE or j<R_SIZE)
+  {
+    if(j>=R_SIZE or (i<L_SIZE and L[i]<R[j]))
+    {
+      arr[k]=L[i];
+      ++i;
+    }
+    else
+    {
+      arr[k]=R[j];
+      ++j;
+    }
+    ++k;
+  }
+
+}
+void mergeSort(std::vector<int> &arr,int left,int right)
+{
+  if(left < right)
+  {
+    int mid =left+(right-left)/2;
+  
+    mergeSort(arr,left,mid);
+    mergeSort(arr,mid+1,right);
+    merge(arr,left,mid,right);
+  }
+}
 //堆排序
 void heapify(std::vector<int> &arr,int n,int i)
 {
@@ -143,6 +206,29 @@ std::vector<int> geneVector(int n)
   std::vector<int> arr(n);
   for (int i = 0; i < n; i++)
     arr[i] = rand();
+  return arr;
+}
+//随机生成大小为n的数组
+std::vector<int> geneVector(int n,int min,int max)
+{
+  assert(n > 0);
+  srand(time(NULL));
+  std::vector<int> arr(n);
+  for (int i = 0; i < n; i++)
+    arr[i] = rand()%(max-min+1)+min;
+  return arr;
+}
+//生成大小为[min,max]的数组,步长为1
+std::vector<int> geneVector(int min,int max)
+{
+  std::vector<int> arr={min};
+  int step=1;
+  if(min>max)
+    step=-1;
+  while(min!=max)
+  {
+    arr.emplace_back(min+=step);
+  }
   return arr;
 }
 //打印数组
